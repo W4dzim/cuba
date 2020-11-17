176e76da0335321ed196d551516527c26bbdd40b
Wadzim <w4dzim@gmail.com>
2020-11-17 22:44:35 +0300

    swiper added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    
+  
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  })
