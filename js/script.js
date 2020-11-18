8a4ff6b117bd5b97a2d19690299075e29d1dec9c
Wadzim <w4dzim@gmail.com>
2020-11-18 22:17:58 +0300

    made adaptive for 'header'

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-const mySwiper = new Swiper('.swiper-container', {
+document.addEventListener("DOMContentLoaded", () => {
+  const mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    
-  
 
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   
+  });
 
+  const menuBtn = document.querySelector('.menu__burger');
+  const menuList = document.querySelector('.menu__list');
+
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('menu__burger--active');
+    menuList.classList.toggle('menu__list--active');
   })
+
+});
